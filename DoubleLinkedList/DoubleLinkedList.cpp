27867fc8b6ddd152cfe33c00d64de9bf82bb62c6
sabilafiridas <sabilafirdias@gmail.com>
2024-05-05 04:34:38 +0700

    membuat kondisi if dalam prosedur addNode

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -19,6 +19,21 @@ void addNode() {
     cin >> newNode->name;               //assign value to the data field of the new node
 
 
+    //insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {      //step 2: insert the new node
+
+        if (START != NULL && newNode->noMhs == START->noMhs) {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
+        //if the list is empty, make the new node the START
+        newNode->next = START;      //step 3: make the new node point to the first node
+        if (START != NULL) {
+            START->prev = newNode;  //step 4: make the first node point to the new node
+        }
+        newNode->prev = NULL;       //step 5: make the new node point to NULL
+        START = newNode;            //step 6: make the new node the first node
+    }
     
 }
 
