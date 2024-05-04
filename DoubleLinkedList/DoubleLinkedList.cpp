b6d33664a955c0392a14b32ffe4880d951e0314e
sabilafiridas <sabilafirdias@gmail.com>
2024-05-05 04:45:05 +0700

    membuat operasi deleteNode

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -71,7 +71,13 @@ bool search(int rollNo, Node** previous, Node** current) {
     return (*current != NULL);
 }
 
+void deleteNode() {
+    Node* previous* current;
+    int rollNo;
 
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; //step 3: get the roll number to be deleted
+}
 
 int main()
 {
