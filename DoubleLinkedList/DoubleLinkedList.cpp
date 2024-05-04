97de9fa210a50f62b3c9141d00d9389c53bad20e
sabilafiridas <sabilafirdias@gmail.com>
2024-05-05 04:35:16 +0700

    membuat kondisi if dalam prosedur addNode

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -34,7 +34,31 @@ void addNode() {
         newNode->prev = NULL;       //step 5: make the new node point to NULL
         START = newNode;            //step 6: make the new node the first node
     }
-    
+    else {
+        //insert the new node in the middle or at the end
+        Node* current = START;  //step 1.a: start from the first node
+        Node* previous = NULL;  //step 1.b: previous node is NULL initially
+
+        while (current != NULL && current->noMhs < newNode->noMhs) {  //step 1.c: traverse
+            previous = current; //step 1.d: move the previous to the current node
+            current = current->next;    //step 1.e: move the current to the next node
+        }
+
+        newNode->next = current;        //step 4: make the next field of the new node point
+        newNode->prev = previous;       //step 5: make the previous field of the new node point
+
+        if (current != NULL) {
+            current->prev = newNode;    //step 6: make the previous field of the current
+        }
+
+        if (previous != NULL) {
+            previous->next = newNode;   //step 7: make the next field of the previous node
+        }
+        else {
+            //if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
+    }
 }
 
 int main()
